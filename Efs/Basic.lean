5a9d787e9763793a7c32a04c79b073f4bc89dc5a
WeAreDevo <g.axelrod1@gmail.com>
2025-12-24 13:56:47 +0200

    substitution

--- Efs/Basic.lean
+++ Efs/Basic.lean
@@ -31,9 +31,32 @@ structure EFSSignature where
 attribute [instance] EFSSignature.K_finite EFSSignature.V_finite EFSSignature.P_finite
 attribute [instance] EFSSignature.K_decEq  EFSSignature.V_decEq  EFSSignature.P_decEq
 
-/-- A term over an EFSSignature is a finite string over the disjoint alphabets K and V. -/
+/- A `K`-string is just a list of `K` symbols. -/
+abbrev KString (S : EFSSignature) : Type := List S.K
+
+/- A nonempty `K`-string. -/
+abbrev NonemptyKString (S : EFSSignature) : Type := { u : KString S // u ≠ [] }
+
+
+/- A term over an EFSSignature is a finite string over the disjoint alphabets K and V. -/
 abbrev Term (S : EFSSignature) : Type :=
   List (S.K ⊕ S.V)
+/-
+Uniform substitution into terms:
+Substitute the nonempty `K`-string `u` for all occurrences of variable `x` in a term.
+-/
+namespace Term
+def subst {S : EFSSignature} (x : S.V) (u : NonemptyKString S) : Term S → Term S
+  | [] => []
+  | (Sum.inl k) :: t =>
+      Sum.inl k :: subst x u t
+  | (Sum.inr y) :: t =>
+      if y = x then -- uses DecidableEq on variables (S.V) to decide y = x.
+        -- replace y by the K-string u, embedded into K⊕V as K-symbols
+        (List.map Sum.inl u.1) ++ subst x u t
+      else
+        Sum.inr y :: subst x u t
+end Term
 
 /-- In Smullyan's presentation, an atomic formula over an EFSSignature is defined as a
 string consisting of a predicate symbol P followed by deg P terms seperated by a
@@ -47,19 +70,43 @@ structure AtomicFormula (S : EFSSignature) where
   P : S.Pred
   args : Vector (Term S) (S.deg P)
 deriving DecidableEq
+/- Lifting substitution to AtomicFormulas -/
+namespace AtomicFormula
+def subst {S : EFSSignature} (x : S.V) (u : NonemptyKString S) :
+  AtomicFormula S → AtomicFormula S
+  | ⟨P, args⟩ =>
+      ⟨P, Vector.map (Term.subst x u) args⟩
 
-/- A direct formulation of Smullyan's definition of
+end AtomicFormula
+
+/- Intuitively, Smullyan's formulas can be seen as Horn-clauses (think Prolog) with
+a finite sequence of premises and a single conclusion.
+A direct formulation of Smullyan's definition of
 well formed formulas might look something like: -/
 -- inductive Formula (S : EFSSignature) where
---   | atom : AtomicFormula S → Formula S
+--   | concl : AtomicFormula S → Formula S
 --   | imp : AtomicFormula S → Formula S → Formula S
-/- But in anticipation of later proofs we instead use the following isomorphic form-/
+/- Note we in effect do not allow the possibilty of non-atomic premises.
+ Intuition why expressive power is not lost:
+ Any implication in a premise can be compiled away by introducing a fresh predicate symbol.
+
+In anticipation of later proofs, instead of a direct formulation,
+ we use the following isomorphic list form that keeps explicit track
+of the premises and single conclusion-/
 structure Formula (S : EFSSignature) where
   premises : List (AtomicFormula S)
   concl    : AtomicFormula S
 deriving DecidableEq
+/- Extending substitution to general Formulas -/
+namespace Formula
+def subst {S : EFSSignature} (x : S.V) (u : NonemptyKString S) :
+  Formula S → Formula S
+  | ⟨premises, concl⟩ =>
+      ⟨List.map (AtomicFormula.subst x u) premises,
+       AtomicFormula.subst x u concl⟩
+end Formula
 
-/--
+/-
 An Elementary Formal System (EFS) over a signature `S`
 is a finite set of formulas (over `S`) called axioms.
 -/
